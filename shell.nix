e5f45f3ec9fa343393225ae40259fbdc12e5e3bc
Ronja Schwarz <71409721+wilhelmines@users.noreply.github.com>
2024-10-27 16:39:53 +0100

    Replaced jupyterlab with marimo

--- shell.nix
+++ shell.nix
@@ -2,13 +2,15 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    nodejs_22
     python312
     python312Packages.pandas
     python312Packages.beautifulsoup4
     python312Packages.requests
-    python312Packages.jupyterlab
     python312Packages.sparqlwrapper
     python312Packages.spotipy
+    python312Packages.bokeh
+    python312Packages.marimo
   ];
 
 PYTHONPATH = "$PWD";  # Sets the current directory in the Python path
